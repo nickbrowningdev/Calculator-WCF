05864f6418e72b12b2a7863e3cf78b8fd454e332
nickbrowningdev <56777652+nickbrowningdev@users.noreply.github.com>
2021-02-04 20:13:49 +1100

    Add files via upload

---
+++ Calculator/CalculatorService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Calculator
+{
+    // this will specify the behaviour of the service and will create a single instance of the service.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class CalculatorService : ICalculatorService
+    {
+        public double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+    }
+}
---
+++ Calculator/ICalculatorService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Calculator
+{
+    // this service contract implemented the events for this class.
+    // For example, this service contract has implemented the event of 
+    // a user to perform basic arithmetric. 
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        // operation contracts are used to define the methods of the service contract
+        // this operation contract is that it will add the numbers that were exchanged
+        [OperationContract]
+        double Add(double n1, double n2);
+
+        // this operation contract is that it will subtract the numbers that were exchanged
+        [OperationContract]
+        double Subtract(double n1, double n2);
+
+        // this operation contract is that it will multiply the numbers that were exchanged
+        [OperationContract]
+        double Multiply(double n1, double n2);
+
+        // this operation contract is that it will divide the numbers that were exchanged
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
